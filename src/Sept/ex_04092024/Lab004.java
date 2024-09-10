ab842745812314284b548155bc41c8d7c5058b46
Poonam Sawant <sawantpoonam683@gmail.com>
2024-09-10 23:01:29 +0530

    added

--- src/Sept/ex_04092024/Lab004.java
+++ src/Sept/ex_04092024/Lab004.java
@@ -1,8 +1,14 @@
 package Sept.ex_04092024;
 
+import java.sql.SQLOutput;
+
 public class Lab004 {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
 
     }
 
