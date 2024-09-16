6ca40070e55a6ab161118e2a74f2ad0ac9a4cd7a
Poonam Sawant <sawantpoonam683@gmail.com>
2024-09-16 23:47:32 +0530

    all files added

---
+++ src/Sept/ex_09092024/Lab009.java
@@ -0,0 +1,14 @@
+package Sept.ex_09092024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+         /*
+    System.out.println("hello");
+    tetsbnkjsdkj
+
+     */
+    }
+
+
+}
---
+++ src/Sept/ex_11092024/Lab012.java
@@ -0,0 +1,16 @@
+package Sept.ex_11092024;
+
+
+public class Lab012 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean $= true;
+        boolean $poonam= true;
+        boolean poonam123= true;
+        boolean  _poonam = true;
+
+        System.out.println(_poonam);
+
+
+    }
+}
