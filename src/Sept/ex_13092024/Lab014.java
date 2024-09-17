3e58c3376d233443a90c81abd184245a8ff5f373
Poonam Sawant <sawantpoonam683@gmail.com>
2024-09-17 23:49:13 +0530

    all files added

---
+++ src/Sept/ex_13092024/Lab014.java
@@ -0,0 +1,23 @@
+package Sept.ex_13092024;
+
+
+import java.sql.SQLOutput;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s %-10s %n", "Name", "Age");
+        System.out.printf("%-10s %-10d %n", "John", 30);
+        System.out.printf("%-10s %-10d %n", "Jane", 25);
+        System.out.printf("%-10s %-10s %n", "Name", "Age");
+        System.out.printf("%-10s %-10d %n", "John", 30);
+        System.out.printf("%-10s %-10d %n", "Jane", 25);
+
+        System.out.printf("Current date: %tD%n", new java.util.Date());
+        double pi = 3.14159;
+        System.out.printf("Pi rounded to two decimals: %.2f%n", pi);
+        // %.2f%n == print the value until two decimals
+
+       
+
+    }
+}
---
+++ src/Sept/ex_13092024/Lab017.java
@@ -0,0 +1,15 @@
+package Sept.ex_13092024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+        int a= 20;
+        int b= 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+
+
+}
