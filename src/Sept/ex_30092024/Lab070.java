3c2ab3dc326320d34f0151cbc1ce8d91c4155725
Poonam Sawant <sawantpoonam683@gmail.com>
2024-10-03 00:11:58 +0530

    all files added -all task  23 sept

---
+++ src/Sept/ex_30092024/Lab070.java
@@ -0,0 +1,15 @@
+package Sept.ex_30092024;
+
+public class Lab070 {
+    public static void main(String[] args) {
+    //Without return type and with parameters
+        poonam("Poonam Sawant");
+
+    }
+    public static void poonam (String name){
+        System.out.println("Without return type and with parameters");
+        System.out.println("Print my name->" + name);
+    }
+
+
+}
---
+++ src/Sept/ex_30092024/Lab071.java
@@ -0,0 +1,17 @@
+package Sept.ex_30092024;
+
+public class Lab071 {
+    public static void main(String[] args) {
+    //With return type and with parameters
+
+        int c =  poonam(5, 10);
+        System.out.println(c);
+
+    }
+    public static int poonam (int a, int b){
+        System.out.println("Without return type and with parameters");
+        return a+b;
+    }
+
+
+}
